6d64416bef0ac91b17428d5c2315873bc7d9158a
TDerudder <113186590+TDerudder@users.noreply.github.com>
2024-01-29 19:38:22 +0100

    BinaryHeapTest

--- code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
+++ code/heap/src/test/java/fr/istic/vv/BinaryHeapTest.java
@@ -1,10 +1,33 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 
 public class BinaryHeapTest {
+
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    boolean popReturnsMinimum(@ForAll List<Integer> elements) {
+        if(elements.isEmpty()) {
+            return true;
+        }
+
+        Comparator<Integer> comparator = Comparator.naturalOrder();
+        BinaryHeap<Integer> heap = new BinaryHeap<>(comparator);
+        elements.forEach(heap::push);
+
+        Integer[] sortedElements = elements.toArray(new Integer[0]);
+        Arrays.sort(sortedElements);
+
+        for(int i = 0; i < sortedElements.length; i++) {
+            Integer min = heap.pop();
+            if(!min.equals(sortedElements[i])) {
+                return false;
+            }
+        }
+        return true;
     }
+
 }
