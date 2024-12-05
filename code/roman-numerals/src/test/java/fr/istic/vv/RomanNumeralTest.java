0893852b39750efe413528c30b904967250d5a8c
Romain Lefeuvre <romain.lefeuvre@inria.fr>
2024-12-05 11:53:16 +0100

    Fix absolute value of Integer.MIN_VALUE in RomanNumeralTest

--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -1,9 +1,10 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
+import net.jqwik.api.constraints.IntRange;
 
 public class RomanNumeralTest {
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
+    boolean absoluteValueOfAllNumbersIsPositive(@ForAll @IntRange(min=Integer.MIN_VALUE +1) int anInteger) {
         return Math.abs(anInteger) >= 0;
     }
 }
