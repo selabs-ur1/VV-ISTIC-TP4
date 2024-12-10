461911214b433dbb241e2837331ec30a04a7643e
asmaa <asmmido48@gmail.com>
2024-12-10 13:17:03 +0100

    implementation

--- code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
+++ code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
@@ -1,11 +1,67 @@
 package fr.istic.vv;
 
 public class RomanNumeraUtils {
+        private static final Map<Character, Integer> Roman_To_Int = new HashMap<>();
+        private static final int Max_Roman = 3999;
+
+        static{
+                Roman_To_Int.put('I', 1);
+                Roman_To_Int.put('V', 5);
+                Roman_To_Int.put('X', 10);
+                Roman_To_Int.put('L', 50);
+                Roman_To_Int.put('C', 100);
+                Roman_To_Int.put('D', 500);
+                Roman_To_Int.put('M', 1000);
+        }
+        public static boolean isValidRomanNumeral(String value) {
+                if (value == null || value.isEmpty()){
+                        return false;
+                }
+                String romanThousand = "M{0,3}";
+                String romanHundred = "C[MD] | D?C{0,3}";
+                String romanTen = " X[CL] | L?X{0,3}";
+                String romanUnit = "I[VX]| V?I{0,3}";
+                String roman = "^" + romanThousand + romanHundred + romanTen + romanUnit + "$";
+
+                return  value.matches(roman);
+        }
     
-        public static boolean isValidRomanNumeral(String value) { return false; }
-    
-        public static int parseRomanNumeral(String numeral) { return 0; }
-    
-        public static String toRomanNumeral(int number) { return ""; }
+        public static int parseRomanNumeral(String numeral) { // XIX
+                if (!isValidRomanNumeral(numeral)){throw new IllegalArgumentException("Invalid Roman numeral");}
+                int result = 0;
+                int previousValue = 0;
+
+                for (int i = numeral.length()-1; i >= 0; i--) {
+                        char currentChar = numeral.charAt(i); // X_I_x
+                        int currentValue = Roman_To_Int.get(currentChar); //10 _1 _10
+
+                        if(currentValue < previousValue){ //result = , result =9 , result =
+                                result -= currentValue;
+                        }else{
+                                result += currentValue;// result = 10, result = , result =19
+                        }
+                        previousValue = currentValue; //10 _ 1- 10
+
+                }
+                return result;
+        }
+
+        public static String toRomanNumeral(int number) { //19
+                if(number <=0||number > Max_Roman){throw new IllegalArgumentException("number out of range");}
+                StringBuilder roman = new StringBuilder();
+
+                int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+                String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+                for (int i = 0; i < values.length; i++) {
+                        while (number >= values[i]) {
+                                roman.append(symbols[i]);
+                                number -= values[i];
+                        }
+                }
+
+                return roman.toString();
+        }
+        }
     
 }
--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -1,10 +1,76 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
 import net.jqwik.api.constraints.IntRange;
+import fr.istic.vv.RomanNumeraUtils;
+
 
 public class RomanNumeralTest {
     @Property
     boolean absoluteValueOfAllNumbersIsPositive(@ForAll @IntRange(min=Integer.MIN_VALUE +1) int anInteger) {
         return Math.abs(anInteger) >= 0;
+    }
+
+    @Property
+    void validRomanNumeralsShouldPassValidation(@ForAll("validRomanNumerals") String numeral) {
+        Assertions.assertTrue(RomanNumeraUtils.isValidRomanNumeral(numeral));
+    }
+
+    @Property
+    void invalidRomanNumeralsShouldFailValidation(@ForAll("invalidRomanNumerals") String numeral) {
+        Assertions.assertFalse(RomanNumeraUtils.isValidRomanNumeral(numeral));
+    }
+
+    @Property
+    void parseRomanNumeralShouldReturnCorrectValue(@ForAll("romanToIntegerCases") Map.Entry<String, Integer> caseEntry) {
+        Assertions.assertEquals(caseEntry.getValue(), RomanNumeraUtils.parseRomanNumeral(caseEntry.getKey()));
+    }
+
+    @Property
+    void toRomanNumeralShouldReturnCorrectString(@ForAll("integerToRomanCases") Map.Entry<Integer, String> caseEntry) {
+        Assertions.assertEquals(caseEntry.getValue(), RomanNumeraUtils.toRomanNumeral(caseEntry.getKey()));
+    }
+
+    @Provide
+    Arbitrary<String> validRomanNumerals() {
+        return Arbitraries.of("I", "IV", "VIII", "IX", "XIV", "XVI", "XCIX", "CV", "MI", "MMCCLXXXIX");
+    }
+
+    @Provide
+    Arbitrary<String> invalidRomanNumerals() {
+        return Arbitraries.of("IIII", "VV", "MMMM", "IC", "VX", "IL", "IXX", "XIIII");
+    }
+
+    @Provide
+    Arbitrary<Map.Entry<String, Integer>> romanToIntegerCases() {
+        Map<String, Integer> cases = Map.of(
+                "I", 1,
+                "IV", 4,
+                "VIII", 8,
+                "IX", 9,
+                "XIV", 14,
+                "XVI", 16,
+                "XCIX", 99,
+                "CV", 105,
+                "MI", 1001,
+                "MMCCLXXXIX", 2289
+        );
+        return Arbitraries.of(cases.entrySet());
+    }
+
+    @Provide
+    Arbitrary<Map.Entry<Integer, String>> integerToRomanCases() {
+        Map<Integer, String> cases = Map.of(
+                1, "I",
+                4, "IV",
+                8, "VIII",
+                9, "IX",
+                14, "XIV",
+                16, "XVI",
+                99, "XCIX",
+                105, "CV",
+                1001, "MI",
+                2289, "MMCCLXXXIX"
+        );
+        return Arbitraries.of(cases.entrySet());
     }
 }
