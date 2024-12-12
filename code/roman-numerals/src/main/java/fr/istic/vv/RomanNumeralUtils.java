582a0b5f3ae06be1a81ac3a0e63e4369a8212853
opalvadeau <olivier.palvadeau@etudiant.univ-rennes1.fr>
2024-12-12 15:09:17 +0100

    done exo1

---
+++ code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeralUtils.java
@@ -0,0 +1,43 @@
+package fr.istic.vv;
+
+import java.util.Map;
+
+public class RomanNumeralUtils {
+    
+        public static boolean isValidRomanNumeral(String value) {
+                return value.matches("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
+        }
+
+        private static final Map<Character, Integer> ROMAN_TO_INT = Map.of('M', 1000,
+                'D', 500, 'C', 100, 'L', 50, 'X', 10, 'V', 5, 'I', 1);
+
+        public static int parseRomanNumeral(String numeral) {//we suppose the input always valid
+                int size = numeral.length();
+                int res = 0;
+                int last = 1000;
+                for(int i = 0; i < size; i++){
+                        char c = numeral.charAt(i);
+                        int value = ROMAN_TO_INT.get(c);
+                        if(last >= value){
+                                res += value;
+                        } else res -= 2*last;
+                }
+                return res;
+        }
+
+        public static String toRomanNumeral(int number) {
+                if(number >= 4000) return "MMMDCCCLXXXVIII";
+                else if (number <= 0) return "";
+                String[] romanSymbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+                int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+                StringBuilder result = new StringBuilder();
+                for (int i = 0; i < values.length; i++) {
+                        while(number >= values[i]) {
+                                result.append(romanSymbols[i]);
+                                number -= values[i];
+                        }
+                }
+                return result.toString();
+        }
+    
+}
--- code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
+++ code/roman-numerals/src/test/java/fr/istic/vv/RomanNumeralTest.java
@@ -2,9 +2,104 @@ package fr.istic.vv;
 import net.jqwik.api.*;
 import net.jqwik.api.constraints.IntRange;
 
+import java.util.Set;
+
 public class RomanNumeralTest {
-    @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll @IntRange(min=Integer.MIN_VALUE +1) int anInteger) {
-        return Math.abs(anInteger) >= 0;
+
+    private static final String MAX_STRING = "MMMDCCCLXXXVIII";
+    private static final int MAX_INT= 3999;
+
+    private static final Set<Character> DIGITS = Set.of('M', 'D', 'C', 'L', 'X', 'V', 'I');
+
+    private static boolean isRomanDigit(char c){
+        return DIGITS.contains(c);
     }
+
+    @Property
+    public boolean cantBeRepeatedDLV(@ForAll @IntRange(max = MAX_INT) int input){
+        String res = RomanNumeralUtils.toRomanNumeral(input);
+        int d = 0, l = 0, v = 0;
+        for(int i = 0; i < res.length(); i++){
+            char c = res.charAt(i);
+            switch(c){
+                case 'D' -> d++;
+                case 'L' -> l++;
+                case 'V' -> v++;
+            }
+            if(d > 1 || l > 1 || v > 1)return false;
+        }
+        return true;
+    }
+
+    @Property
+    public boolean cantBeRepeated3TimesMCXI(@ForAll @IntRange(max = MAX_INT) int input){
+        String res = RomanNumeralUtils.toRomanNumeral(input);
+        int m = 0, c = 0, x =0, i = 0;
+        char last = 'T';
+        for(int j = 0; j < res.length(); j++){
+            char ch = res.charAt(j);
+            if(last != ch) { m = 0; c = 0; x = 0; i = 0;}
+            switch(ch){
+                case 'M' -> m++;
+                case 'C' -> c++;
+                case 'X' -> x++;
+                case 'I' -> i++;
+            }//LXV
+            if(m > 3 || c > 3 || x > 3 || i > 3)return false;
+            last = ch;
+        }
+        return true;
+    }
+
+    @Property
+    public boolean onlyRomanDigits(@ForAll @IntRange(max = MAX_INT) int input){
+        String res = RomanNumeralUtils.toRomanNumeral(input);
+        for(int i = 0; i < res.length(); i++){
+            if(!isRomanDigit(res.charAt(i)))return false;
+        }
+        return true;
+    }
+
+    @Property
+    public boolean romanNumberAlwaysUnderMax(@ForAll @IntRange(max = MAX_INT) int input){
+        String res = RomanNumeralUtils.toRomanNumeral(input);
+        int size = res.length();
+        return size <= MAX_STRING.length();
+    }
+
+    //for integer generation
+
+    public Arbitrary<String> romanThreeChars(char c){
+        return Arbitraries.strings().ofMaxLength(3).withChars(c);
+    }
+
+    public Arbitrary<String> romanOneChar(char c){
+        return Arbitraries.strings().ofMaxLength(1).withChars(c);
+    }
+
+    @Provide
+    public Arbitrary<String> romanString(){
+        return Combinators.combine(romanThreeChars('M'),
+                romanOneChar('D'),
+                romanThreeChars('C'),
+                romanOneChar('L'),
+                romanThreeChars('X'),
+                romanOneChar('V'),
+                romanThreeChars('I')).as(
+                (m,d,c,l,x,v,i) -> m+d+c+l+x+v+i);
+    }
+
+    @Property
+    public boolean numberAlwaysInBounds(@ForAll @From("romanString") String input){
+        int res = RomanNumeralUtils.parseRomanNumeral(input);
+        return res >= 0 && res <= 3999;
+    }
+
+    //for isRoman
+
+    @Property
+    public boolean OneRomanCharAlwaysRoman(@ForAll @From("romanString") String input){
+        return RomanNumeralUtils.isValidRomanNumeral(input);
+    }
+
 }
