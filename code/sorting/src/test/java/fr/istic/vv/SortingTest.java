ec6dec9f98b5c58b0a6473f22a75b3235470b4f0
Killian2001 <killianc2001@gmail.com>
2024-12-15 01:28:11 +0100

    first properties for bubble sort.

--- code/sorting/src/test/java/fr/istic/vv/SortingTest.java
+++ code/sorting/src/test/java/fr/istic/vv/SortingTest.java
@@ -1,9 +1,33 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class SortingTest {
+
+    private static final Comparator<Integer> INT_COMPARATOR = Integer::compareTo;
+
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    boolean isSorted(@ForAll Integer[] array) {
+        Integer[] sorted = Sorting.bubblesort(array, INT_COMPARATOR);
+
+        for (int i = 1; i < sorted.length; i++)
+            if (INT_COMPARATOR.compare(sorted[i - 1], sorted[i]) > 0)
+                return false;
+
+        return true;
+    }
+
+    @Property
+    boolean containsAll(@ForAll Integer[] array) {
+        Integer[] sorted = Sorting.bubblesort(array, INT_COMPARATOR);
+
+        return Arrays.stream(sorted).allMatch(element -> Arrays.asList(array).contains(element));
+    }
+
+    @Property
+    boolean sameSize(@ForAll Integer[] array) {
+        return Sorting.bubblesort(array, INT_COMPARATOR).length == array.length;
     }
 }
