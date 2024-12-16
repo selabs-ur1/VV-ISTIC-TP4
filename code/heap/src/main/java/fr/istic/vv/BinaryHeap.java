06f068460e02122b56fc32df04b61e65469f2275
Killian2001 <killianc2001@gmail.com>
2024-12-16 17:54:36 +0100

    proposed a first implementation for binary heap.

--- code/heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -1,17 +1,114 @@
 package fr.istic.vv;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class BinaryHeap<T> {
 
-    public BinaryHeap(Comparator<T> comparator) {  }
+    private final Comparator<T> comparator;
+    private final List<T> heap = new ArrayList<>();
 
-    public T pop() { return null; }
+    private int count = 0;
 
-    public T peek() { return null; }
+    public BinaryHeap(Comparator<T> comparator) {
+        this.comparator = comparator;
+    }
 
-    public void push(T element) { }
+    public T pop() {
+        if (count == 0)
+            throw new NoSuchElementException("the heap is currently empty");
 
-    public int count() { return 0; }
+        T result = heap.get(0);
+
+        // We move the last element of the heap to the root.
+        heap.add(heap.getLast());
+        heap.removeLast();
+
+        // Then, we "percolate it down" to preserve heap property.
+
+        int currIndex = 0;              // Newly added element index.
+        int leftIndex = 1;      // Index of its child parent.
+        int rightIndex = 2;
+        T currKey = heap.get(currIndex);      // Key of newly added element.
+
+        // Swap current key and child with the smaller key, until we reach a leaf.
+        while (leftIndex < count || rightIndex < count) {
+            int indexToSwap;    // Index of the key to swap with the current key.
+            if (leftIndex < count && rightIndex < count) {
+                // If there is two child, choose the smallest child.
+                int leftRightCmp =
+                        comparator.compare(heap.get(leftIndex), heap.get(rightIndex));
+                indexToSwap = leftRightCmp < 0 ? leftIndex : rightIndex;
+
+                // If the child key is bigger than the current key, use the other child key.
+                if (comparator.compare(currKey, heap.get(indexToSwap)) < 0)
+                    indexToSwap = leftRightCmp < 0 ? rightIndex : leftRightCmp;
+            } else if (rightIndex >= count) // ...otherwise choose the only possible child.
+                indexToSwap = leftIndex;
+            else
+                indexToSwap = rightIndex;
+
+            T valueToSwap = heap.get(indexToSwap);
+
+            // If the chosen key is still greater than the current key, stop the percolation.
+            if (comparator.compare(currKey, valueToSwap) < 0)
+                break;
+
+            // Swap the keys.
+            heap.set(currIndex, valueToSwap);
+            heap.set(indexToSwap, currKey);
+
+            // Update indices.
+            currIndex = indexToSwap;
+            leftIndex = currIndex * 2 + 1;
+            rightIndex = currIndex * 2 + 2;
+        }
+
+        count--;
+        return result;
+    }
+
+    public T peek() {
+        if (count == 0)
+            throw new NoSuchElementException("the heap is currently empty");
+
+        return heap.get(0);
+    }
+
+    public void push(T element) {
+        // Add the element to the heap.
+        heap.add(element);
+        count++;
+
+        int currIndex = count - 1;  // Newly added element index.
+        int parentIndex;            // Index of its parent.
+        T currKey;                  // Key of newly added element.
+        T parentKey;                // Key of its parent.
+
+        // Swapping current and parent until we reach the root or the tree or we encounter a smaller
+        // parent key.
+        while (currIndex != 0) {
+            parentIndex = (currIndex - 1) / 2;
+
+            currKey = heap.get(currIndex);
+            parentKey = heap.get(parentIndex);
+
+            // We stop if the parent's key is smaller.
+            if (comparator.compare(currKey, parentKey) >= 0)
+                break;
+
+            // ...otherwise we swap the keys.
+            heap.set(currIndex, parentKey);
+            heap.set(parentIndex, currKey);
+
+            currIndex = count - 1;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
 
 }
