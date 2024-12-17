7041d17f65313c06364d97507e158fb89b15d7fa
Ekodge <Ethan.cadorel@hotmail.fr>
2024-12-17 17:52:00 +0100

    fin du tp

--- code/heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -1,17 +1,74 @@
 package fr.istic.vv;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.NoSuchElementException;
 
 public class BinaryHeap<T> {
+    private final ArrayList<T> heap;
+    private final Comparator<T> comparator;
 
-    public BinaryHeap(Comparator<T> comparator) {  }
+    public BinaryHeap(Comparator<T> comparator) {
+        this.comparator = comparator;
+        this.heap = new ArrayList<>();
+    }
 
-    public T pop() { return null; }
+    public T pop() {
+        if (heap.isEmpty()) throw new NoSuchElementException();
+        T root = heap.get(0);
+        T lastElement = heap.remove(heap.size() - 1);
+        if (!heap.isEmpty()) {
+            heap.set(0, lastElement);
+            siftDown(0);
+        }
+        return root;
+    }
 
-    public T peek() { return null; }
+    public T peek() {
+        if (heap.isEmpty()) throw new NoSuchElementException();
+        return heap.get(0);
+    }
 
-    public void push(T element) { }
+    public void push(T element) {
+        heap.add(element);
+        siftUp(heap.size() - 1);
+    }
 
-    public int count() { return 0; }
+    public int count() {
+        return heap.size();
+    }
 
+    private void siftUp(int index) {
+        while (index > 0) {
+            int parentIndex = (index - 1) / 2;
+            if (comparator.compare(heap.get(index), heap.get(parentIndex)) >= 0) break;
+            swap(index, parentIndex);
+            index = parentIndex;
+        }
+    }
+
+    private void siftDown(int index) {
+        int size = heap.size();
+        while (true) {
+            int leftChild = 2 * index + 1;
+            int rightChild = 2 * index + 2;
+            int smallest = index;
+
+            if (leftChild < size && comparator.compare(heap.get(leftChild), heap.get(smallest)) < 0) {
+                smallest = leftChild;
+            }
+            if (rightChild < size && comparator.compare(heap.get(rightChild), heap.get(smallest)) < 0) {
+                smallest = rightChild;
+            }
+            if (smallest == index) break;
+            swap(index, smallest);
+            index = smallest;
+        }
+    }
+
+    private void swap(int i, int j) {
+        T temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
 }
