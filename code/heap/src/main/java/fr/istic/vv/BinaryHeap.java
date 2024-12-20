5662231488eb2e736cbad3720a3ed13b90c38a5c
yelmellali <yasser.elmellali@etudiant.univ-rennes1.fr>
2024-12-20 19:38:48 +0100

    Tp4 fait

--- code/heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -1,17 +1,87 @@
 package fr.istic.vv;
 
 import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class BinaryHeap<T> {
+    private final Comparator<T> comparator;
+    private final List<T> heap;
 
-    public BinaryHeap(Comparator<T> comparator) {  }
+    public BinaryHeap(Comparator<T> comparator) {
+        this.comparator = comparator;
+        this.heap = new ArrayList<>();
+    }
 
-    public T pop() { return null; }
+    public void push(T element) {
+        heap.add(element);
+        siftUp(heap.size() - 1);
+    }
 
-    public T peek() { return null; }
+    public T pop() {
+        if (heap.isEmpty()) {
+            throw new NoSuchElementException("Heap is empty");
+        }
+        T minElement = heap.get(0);
+        T lastElement = heap.remove(heap.size() - 1);
+        if (!heap.isEmpty()) {
+            heap.set(0, lastElement);
+            siftDown(0);
+        }
+        return minElement;
+    }
 
-    public void push(T element) { }
+    public T peek() {
+        if (heap.isEmpty()) {
+            throw new NoSuchElementException("Heap is empty");
+        }
+        return heap.get(0);
+    }
 
-    public int count() { return 0; }
+    public int count() {
+        return heap.size();
+    }
 
+    private void siftUp(int index) {
+        while (index > 0) {
+            int parentIndex = (index - 1) / 2;
+            if (comparator.compare(heap.get(index), heap.get(parentIndex)) < 0) {
+                swap(index, parentIndex);
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void siftDown(int index) {
+        int leftChildIndex;
+        int rightChildIndex;
+        int smallest;
+
+        while ((leftChildIndex = 2 * index + 1) < heap.size()) {
+            rightChildIndex = leftChildIndex + 1;
+            smallest = index;
+
+            if (comparator.compare(heap.get(leftChildIndex), heap.get(smallest)) < 0) {
+                smallest = leftChildIndex;
+            }
+            if (rightChildIndex < heap.size() && comparator.compare(heap.get(rightChildIndex), heap.get(smallest)) < 0) {
+                smallest = rightChildIndex;
+            }
+            if (smallest != index) {
+                swap(index, smallest);
+                index = smallest;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void swap(int i, int j) {
+        T temp = heap.get(i);
+        heap.set(i, heap.get(j));
+        heap.set(j, temp);
+    }
 }
--- code/sorting/src/test/java/fr/istic/vv/SortingTest.java
+++ code/sorting/src/test/java/fr/istic/vv/SortingTest.java
@@ -1,9 +1,60 @@
 package fr.istic.vv;
 import net.jqwik.api.*;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class SortingTest {
+
+    private static <T> boolean isSorted(T[] array, Comparator<T> comparator) {
+        for (int i = 1; i < array.length; i++) {
+            if (comparator.compare(array[i - 1], array[i]) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Property
-    boolean absoluteValueOfAllNumbersIsPositive(@ForAll int anInteger) {
-        return Math.abs(anInteger) >= 0;
+    boolean bubbleSortWorksCorrectly(@ForAll int[] array) {
+        Integer[] inputArray = Arrays.stream(array).boxed().toArray(Integer[]::new);
+        Comparator<Integer> comparator = Integer::compareTo;
+
+        Integer[] sortedArray = Sorting.bubbleSort(inputArray, comparator);
+
+        return isSorted(sortedArray, comparator);
+    }
+
+    @Property
+    boolean quickSortWorksCorrectly(@ForAll int[] array) {
+        Integer[] inputArray = Arrays.stream(array).boxed().toArray(Integer[]::new);
+        Comparator<Integer> comparator = Integer::compareTo;
+
+        Sorting.quickSort(inputArray, comparator);
+
+        return isSorted(inputArray, comparator);
+    }
+
+    @Property
+    boolean mergeSortWorksCorrectly(@ForAll int[] array) {
+        Integer[] inputArray = Arrays.stream(array).boxed().toArray(Integer[]::new);
+        Comparator<Integer> comparator = Integer::compareTo;
+
+        Integer[] sortedArray = Sorting.mergeSort(inputArray, comparator);
+
+        return isSorted(sortedArray, comparator);
+    }
+
+    @Property
+    boolean allSortsReturnTheSameResult(@ForAll int[] array) {
+        Integer[] inputArray = Arrays.stream(array).boxed().toArray(Integer[]::new);
+        Comparator<Integer> comparator = Integer::compareTo;
+
+        Integer[] bubbleSorted = Sorting.bubbleSort(inputArray.clone(), comparator);
+        Integer[] quickSorted = inputArray.clone();
+        Sorting.quickSort(quickSorted, comparator);
+        Integer[] mergeSorted = Sorting.mergeSort(inputArray.clone(), comparator);
+
+        return Arrays.equals(bubbleSorted, quickSorted) && Arrays.equals(bubbleSorted, mergeSorted);
     }
 }
