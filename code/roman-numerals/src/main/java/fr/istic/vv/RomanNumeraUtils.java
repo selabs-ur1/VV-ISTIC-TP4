0b3510eae7856ee5748b8126edbe06f3da5b2287
kkowal <kenzo.kowal@etudiant.univ-rennes1.fr>
2024-12-20 23:09:27 +0100

    les tests des sorts a faire et heap à faire

--- code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
+++ code/roman-numerals/src/main/java/fr/istic/vv/RomanNumeraUtils.java
@@ -1,11 +1,126 @@
 package fr.istic.vv;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RomanNumeraUtils {
-    
-        public static boolean isValidRomanNumeral(String value) { return false; }
-    
-        public static int parseRomanNumeral(String numeral) { return 0; }
-    
-        public static String toRomanNumeral(int number) { return ""; }
-    
+
+        public static boolean isValidRomanNumeral(String value) {
+
+                Pattern pattern = Pattern.compile("^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$",
+                                Pattern.CASE_INSENSITIVE);
+                Matcher matcher = pattern.matcher(value);
+                return matcher.find();
+        }
+
+        public static int parseRomanNumeral(String numeral) {
+                if (isValidRomanNumeral(numeral)) {
+                        int res = 0;
+                        for (int i = 0; i < numeral.length(); i++) {
+                                switch (numeral.charAt(i)) {
+                                        case 'I': {
+                                                if (i == numeral.length() - 1) {
+                                                        if (numeral.charAt(i + 1) == 'V') {
+                                                                res += 4;
+                                                                i++;
+                                                                break;
+
+                                                        } else if (numeral.charAt(i + 1) == 'X') {
+                                                                res += 9;
+                                                                i++;
+                                                                break;
+                                                        }
+                                                }
+                                                res += 1;
+                                                break;
+                                        }
+                                        case 'V':
+                                                res += 5;
+                                                break;
+                                        case 'X': {
+                                                if (i == numeral.length() - 1) {
+                                                        if (numeral.charAt(i + 1) == 'C') {
+                                                                res += 90;
+                                                                i++;
+                                                                break;
+                                                        }
+                                                }
+                                                res += 10;
+                                                break;
+                                        }
+                                        case 'L':
+                                                res += 50;
+                                                break;
+                                        case 'C':
+                                                res += 100;
+                                                break;
+                                        case 'D':
+                                                res += 500;
+                                                break;
+                                        case 'M':
+                                                res += 1000;
+                                                break;
+                                        default:
+                                                break;
+                                }
+                        }
+                        return res;
+                } else {
+                        return -1;
+                }
+        }
+
+        public static String toRomanNumeral(int number) {
+                if (number < 1 || number > 3999) {
+                        return "Invalid Roman Number Value";
+                }
+                String romanValue = "";
+                int n = number;
+                while (n > 0) {
+                        if (n >= 1000) {
+                                romanValue += "M";
+                                n -= 1000;
+                        } else if (n >= 900) {
+                                romanValue += "CM";
+                                n -= 900;
+                        } else if (n >= 500) {
+                                romanValue += "D";
+                                n -= 500;
+                        } else if (n >= 400) {
+                                romanValue += "CD";
+                                n -= 400;
+                        } else if (n >= 100) {
+                                romanValue += "C";
+                                n -= 100;
+                        } else if (n >= 90) {
+                                romanValue += "XC";
+                                n -= 90;
+                        } else if (n >= 50) {
+                                romanValue += "L";
+                                n -= 50;
+                        } else if (n >= 40) {
+                                romanValue += "XL";
+                                n -= 40;
+                        } else if (n >= 10) {
+                                romanValue += "X";
+                                n -= 10;
+                        } else if (n >= 9) {
+                                romanValue += "IX";
+                                n -= 9;
+                        } else if (n >= 5) {
+                                romanValue += "V";
+                                n -= 5;
+                        } else if (n >= 4) {
+                                romanValue += "IV";
+                                n -= 4;
+                        } else if (n >= 1) {
+                                romanValue += "I";
+                                n -= 1;
+                        }
+                }
+                return romanValue;
+        }
+
 }
--- code/sorting/src/main/java/fr/istic/vv/Sorting.java
+++ code/sorting/src/main/java/fr/istic/vv/Sorting.java
@@ -3,10 +3,98 @@ import java.util.Comparator;
 
 public class Sorting {
 
-    public static <T>  T[] bubblesort(T[] array, Comparator<T> comparator) { return null; }
+    public static <T>  T[] bubblesort(T[] array, Comparator<T> comparator) { 
+        T[] arrayToSort = array.clone();
+        for (int i = 0; i < arrayToSort.length; i++) {
+            for (int j = 0; j < i-1; j++) {
+                if(comparator.compare(arrayToSort[j], arrayToSort[j+1]) > 0) {
+                    T temp = arrayToSort[j];
+                    arrayToSort[j] = arrayToSort[j+1];
+                    arrayToSort[j+1] = temp;
+                }
+            }
+        }
+        return arrayToSort;
+    }
 
-    public static <T> T[] quicksort(T[] array, Comparator<T> comparator)  { return null; }
+    public static <T> T[] quicksort(T[] array, Comparator<T> comparator)  { 
+        T[] arrayToSort = array.clone();
+        quicksortFirst(arrayToSort,comparator,0,arrayToSort.length);
+        return arrayToSort;
+    }
+    public static <T> void quicksortFirst(T[] array, Comparator<T> comparator,int begin, int end)  { 
+        if(begin<end){
+            int partitionIndex = partition(array, begin, end,comparator);
 
-    public static <T> T[] mergesort(T[] array, Comparator<T> comparator) { return null; }
+        quicksortFirst(array,comparator, begin, partitionIndex-1);
+        quicksortFirst(array,comparator, partitionIndex+1, end);
+        }
+    }
+    public static <T> int partition(T[] array,int begin,int end,Comparator<T> comparator)  {
+        T pivot = array[end];
+        int i = begin-1;
 
+        for (int j = begin; j < end; j++) {
+            if(comparator.compare(array[j], pivot) < 0){
+                i++;
+
+                T swapTemp = array[i];
+                array[i] = array[j];
+                array[j] = swapTemp;
+            }
+        }
+
+        T swapTemp  = array[i+1];
+        array[i+1] = array[end];
+        array[end] = swapTemp;
+
+        return i++;
+    }
+
+
+    public static <T> T[] mergesort(T[] array, Comparator<T> comparator) {
+        if (array == null || array.length < 2) {
+            return array; 
+        }
+    
+        T[] arrayToSort = array.clone();
+        T[] tempArray = array.clone(); 
+        mergesortHelper(arrayToSort, tempArray, 0, arrayToSort.length - 1, comparator);
+        return arrayToSort;
+    }
+    
+
+    private static <T> void mergesortHelper(T[] array, T[] tempArray, int left, int right, Comparator<T> comparator) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+    
+            mergesortHelper(array, tempArray, left, middle, comparator);
+            mergesortHelper(array, tempArray, middle + 1, right, comparator);
+    
+            merge(array, tempArray, left, middle, right, comparator);
+        }
+    }
+    
+    private static <T> void merge(T[] array, T[] tempArray, int left, int middle, int right, Comparator<T> comparator) {
+    
+        int i = 0;       
+        int j = 0;
+        int k = 0;
+    
+        while (i <= middle && j <= right) {
+            if (comparator.compare(tempArray[i], tempArray[j]) <= 0) {
+                array[k++] = tempArray[i++];
+            } else {
+                array[k++] = tempArray[j++];
+            }
+        }
+    
+        while (i < left) {
+            array[k++] = tempArray[i++];
+        }
+        while (i < right) {
+            array[k++] = tempArray[i++];
+        }
+    }
+    
 }
